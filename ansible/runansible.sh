8294eebba08dac47469e2a9912dbc012b1ef6054
Timothy Vandenbrande <timothy.vandenbrande@cegeka.com>
2019-04-04 11:52:06 +0200

    more dynamic uid fixes

--- ansible/runansible.sh
+++ ansible/runansible.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 cd /thycotic
 
 ansible-playbook generate_openshift_secrets.yml --connection=local -vvv
